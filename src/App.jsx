3d96b36d262c5b300e1400fe671bf6c2917e6c0f
Jorge <jorgelarteagap@gmail.com>
2022-02-17 19:28:33 -0400

    [STYLE] Skills, about, services style

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,12 @@ import {
   BiBriefcase,
   BiMessageSquare,
   BiMoon,
-  BiMouse
+  BiMouse,
+  BiAward,
+  BiBriefcaseAlt,
+  BiSupport,
+  BiBadgeCheck,
+  BiRightArrowAlt
 } from 'react-icons/bi'
 import { BsLinkedin, BsGithub, BsTwitter } from 'react-icons/bs'
 function App() {
@@ -86,8 +91,189 @@ function App() {
             </a>
           </div>
         </section>
-        <section className="home section" id="about"></section>
-        <section className="home section" id="skills"></section>
+        <section className="home section" id="about">
+          <span className="section__subtitle">My intro</span>
+          <h2 className="section__title">About Me</h2>
+
+          <div className="about__container container grid">
+            <img src="" alt="About me" className="about__img" />
+            <div className="about__data">
+              <div className="about__info">
+                <div className="about__box">
+                  <BiAward className="about__icon" />
+                  <h3 className="about__title">Experience</h3>
+                  <span className="about__subtitle">8 Years Working</span>
+                </div>
+                <div className="about__box">
+                  <BiBriefcaseAlt className="about__icon" />
+                  <h3 className="about__title">Complete</h3>
+                  <span className="about__subtitle">48 + projects</span>
+                </div>
+                <div className="about__box">
+                  <BiSupport className="about__icon" />
+                  <h3 className="about__title">Support</h3>
+                  <span className="about__subtitle">Online 24/7</span>
+                </div>
+              </div>
+              <p className="about__description">
+                Frontend Developer with experience in layout prototypes,
+                interested in deploying proposals and designed with tools like
+                Figma, achieving user-friendly websites with languages like
+                javascript (React, Node) taking into account UX strategies
+                <br />
+                Also working with backend technologies with Django and Node.
+                Databases including MongoDB and GraphQL
+              </p>
+              <a href="#contact" className="button">
+                Contact me
+              </a>
+            </div>
+          </div>
+        </section>
+
+        <section className="home section" id="skills">
+          <span className="section__subtitle">My abilities</span>
+          <h2 className="section__title">My Experience</h2>
+
+          <div className="skills__container container grid">
+            <div className="skills__content">
+              <h3 className="skills__title">Frontend developer</h3>
+              <div className="skills__box">
+                <div className="skills__group">
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">Html</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">CSS3</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">JavaScript</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                </div>
+                <div className="skills__group">
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">React</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">Bootstrap</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">Bulma CSS</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="skills__content">
+              <h3 className="skills__title">Backend developer</h3>
+              <div className="skills__box">
+                <div className="skills__group">
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">Node Js</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">Python / Django</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">GraphQl</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                </div>
+                <div className="skills__group">
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">MySql</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">MongoDB</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                  <div className="skills__data">
+                    <BiBadgeCheck className="skills__icon" />
+                    <div>
+                      <h3 className="skills__name">Firebase</h3>
+                      <span className="skills__level"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="home section" id="services">
+          <span className="section__subtitle">My Services</span>
+          <h2 className="section__title">What I Offer</h2>
+
+          <div className="services__container container grid">
+            <div className="services__card">
+              <h3 className="services__title">
+                Product <br /> Designer
+              </h3>
+              <span className="services__button">
+                See more <BiRightArrowAlt className="services__icon" />
+              </span>
+            </div>
+            <div className="services__card">
+              <h3 className="services__title">
+                UI/UX <br />
+                Designer
+              </h3>
+              <span className="services__button">
+                See more <BiRightArrowAlt className="services__icon" />
+              </span>
+            </div>
+            <div className="services__card">
+              <h3 className="services__title">
+                Visual <br />
+                Designer
+              </h3>
+              <span className="services__button">
+                See more <BiRightArrowAlt className="services__icon" />
+              </span>
+            </div>
+          </div>
+        </section>
+        <section className="home section" id="work"></section>
       </main>
       <footer className="footer"></footer>
       <Article />
