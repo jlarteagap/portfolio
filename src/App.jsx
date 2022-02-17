366e873cbfd2cfc42919d51758cc5ea62b1af03e
Jorge <jorgelarteagap@gmail.com>
2022-02-17 17:24:36 -0400

    [STYLE] Header movil

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import Article from './components/articles/Articles'
+import {
+  BiHomeAlt,
+  BiUser,
+  BiBook,
+  BiBriefcase,
+  BiMessageSquare,
+  BiMoon
+} from 'react-icons/bi'
 
 function App() {
   return (
     <div className="App">
+      <div className="header" id="header">
+        <nav className="nav container">
+          <a href="#" className="nav__logo">
+            Jorge Arteaga
+          </a>
+          <div className="nav__menu">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <a href="#" className="nav__link active-link">
+                  <BiHomeAlt />
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  <BiUser />
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  <BiBook />
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  <BiBriefcase />
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  <BiMessageSquare />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <BiMoon />
+        </nav>
+      </div>
       <Article />
     </div>
   )
