f1b6a2dbbfe5636be714b2eb45a76a8a9e0b5353
Jorge <jorgelarteagap@gmail.com>
2022-02-17 18:14:55 -0400

    [STYLE] home style

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ import {
   BiBook,
   BiBriefcase,
   BiMessageSquare,
-  BiMoon
+  BiMoon,
+  BiMouse
 } from 'react-icons/bi'
-
+import { BsLinkedin, BsGithub, BsTwitter } from 'react-icons/bs'
 function App() {
   return (
     <div className="App">
@@ -49,6 +50,46 @@ function App() {
           <BiMoon />
         </nav>
       </div>
+      <main className="main">
+        <section className="home section" id="home">
+          <div className="home__container container grid">
+            <div className="home_data">
+              <span className="home__greeting">Hello, I&#39;m</span>
+              <h1 className="home__name">Jorge Arteaga</h1>
+              <h3 className="home__education">Frontend Developer</h3>
+            </div>
+            <div className="home__buttons">
+              <a download="" href="#" className="button button--ghost">
+                Download CV
+              </a>
+              <a href="#about" className="button">
+                About me
+              </a>
+            </div>
+            <div className="home__handle">
+              <img src="" alt="Perfil" className="home__img" />
+            </div>
+            <div className="home__social">
+              <a href="" className="home__social-link">
+                <BsLinkedin />
+              </a>
+              <a href="" className="home__social-link">
+                <BsGithub />
+              </a>
+              <a href="" className="home__social-link">
+                <BsTwitter />
+              </a>
+            </div>
+            <a href="#about" className="home__scroll">
+              <BiMouse className="home__scroll-icon" />
+              <span className="home__scroll-name">Scroll Down</span>
+            </a>
+          </div>
+        </section>
+        <section className="home section" id="about"></section>
+        <section className="home section" id="skills"></section>
+      </main>
+      <footer className="footer"></footer>
       <Article />
     </div>
   )
