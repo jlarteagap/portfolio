21aece200aee04f312d8f3abddcd0b187e587f15
Jorge <jorgelarteagap@gmail.com>
2022-11-28 22:09:15 -0400

    firts steps from blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import './public/style.css'
 import ThemeContextWrapper from '../src/theme/ThemeContextWrapper'
 import Layout from '../src/components/layout'
+import 'bulma/css/bulma.min.css';
+import 'public/style.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
-function Blog(){
-    return('Hola')
+function Blog() {
+  return (
+    <section className="container blog_content">
+      <article className="content ">
+        <header>
+          <h2 className="is-size-2 blog_title">
+            SEO On-Page en las principales eCommerce en Santa Cruz - Bolivia
+          </h2>
+        </header>
+        <section className="blog_body">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque amet
+            aliquid est tempora incidunt omnis, fuga libero tenetur placeat ex
+            sapiente nemo maxime nostrum id ratione. Nulla quibusdam
+            perspiciatis quisquam.
+          </p>
+        </section>
+        <footer>Footer</footer>
+      </article>
+      <aside className="blog_aside">
+        Sidebar Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatem ducimus voluptatibus eos, tenetur, omnis non expedita at sunt
+        iusto officiis, velit quaerat sit officia aperiam laborum laudantium
+        ratione eligendi molestiae?
+      </aside>
+    </section>
+  );
 }
 
-export default Blog
+export default Blog;
