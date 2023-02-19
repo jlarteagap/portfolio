da8c383f4086ca6cfd219a49e82fec6add6e3944
Jorge <jorgelarteagap@gmail.com>
2023-02-19 18:13:39 -0400

    [FIX] Minor fixes

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Work from '../src/components/work'
 import Contact from '../src/components/contact'
 import BlogList from '../src/components/blog/BlogList'
 
-const index = ({ data }) => {
+const index = () => {
   return (
     <>
       <Home siteTitle="Jorge Arteaga" />
@@ -23,14 +23,3 @@ const index = ({ data }) => {
 }
 
 export default index
-
-export async function getServerSideProps() {
-  const res = await fetch('http://localhost:3000/api/about')
-  const data = await res.json()
-
-  return {
-    props: {
-      data
-    }
-  }
-}
